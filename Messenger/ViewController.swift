d3323b20034188ea8b145fbcca76915ee30b605d
kadir mert yildiz <kadirmert.34@hotmail.com>
2024-09-11 20:10:15 +1000

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Kadir Yildiz on 11/9/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
